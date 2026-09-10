5870738665269dff23a09199711399e43cff5315
dima648228 <stalker2280071337@gmail.com>
2026-09-10 12:44:08 +0300

    implemented solution for 1st lab

--- lab_01.py
+++ lab_01.py
@@ -4,22 +4,23 @@
 
 # already sorted list
 list = [
-    {"name":"Bob", "phone":"0631234567"},
-    {"name":"Emma", "phone":"0631234567"},
-    {"name":"Jon",  "phone":"0631234567"},
-    {"name":"Zak",  "phone":"0631234567"}
+    {"name":"Bob", "phone":"0631234567", "email":"bob3214@gmail.com"},
+    {"name":"Emma", "phone":"0631234567", "email":"emma@ukr.net"},
+    {"name":"Jon",  "phone":"0631234567", "email":"jon00@gmail.com"},
+    {"name":"Zak",  "phone":"0631234567", "email":"zak44@gmail.com"}
 ]
 
 def printAllList():
     for elem in list:
-        strForPrint = "Student name is " + elem["name"] + ",  Phone is " + elem["phone"]
+        strForPrint = "Student name is " + elem["name"] + ",  Phone is " + elem["phone"] + ", Email is " + elem["email"]
         print(strForPrint)
     return
 
 def addNewElement():
     name = input("Pease enter student name: ")
     phone = input("Please enter student phone: ")
-    newItem = {"name": name, "phone": phone}
+    email = input("Plase enter student email: ")
+    newItem = {"name": name, "phone": phone, "email": email}
     # find insert position
     insertPosition = 0
     for item in list:
@@ -48,7 +49,34 @@ def deleteElement():
 
 def updateElement():
     name = input("Please enter name to be updated: ")
-    # implementation required
+    updatePosition = -1
+    for item in list:
+        if name == item["name"]:
+            updatePosition = list.index(item)
+            break
+    if updatePosition == -1:
+        print("Element was not found")
+    else:
+        update = ""
+        
+        choice = int(input("What should be updated (1 - name, 2 - phone, 3 - email): "))
+        if choice == 1:
+            update = "name"
+        elif choice == 2:
+            update = "phone"
+        elif choice == 3:
+            update = "email"
+
+        if update == "":
+            print("Incorrect choice")
+            
+            return
+
+        res = input("Enter new value for " + update + ": ")
+        item[update] = res
+
+        list.sort(key=lambda user: user["name"])
+    return
 
 def main():
     while True:
@@ -60,9 +88,12 @@ def main():
                 printAllList()
             case "U" | "u":
                 print("Existing element will be updated")
+                updateElement()
+                printAllList()
             case "D" | "d":
                 print("Element will be deleted")
                 deleteElement()
+                printAllList()
             case "P" | "p":
                 print("List will be printed")
                 printAllList()
@@ -71,3 +102,5 @@ def main():
                 break
             case _:
                 print("Wrong chouse")
+
+main()
